268bbc6543ba5472e0b673a3a4fd0f5df6269dcd
MarisKX <maris.kurpnieks@gmail.com>
2021-12-02 17:37:16 +0000

    Created sticky nav bar with jQuery and CSS

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
 });
+
+
+$(window).scroll(function () {
+    // add 'sticky-header' class to the body tag when the header top is scrolled out of view
+    $('#main-navbar').toggleClass('sticky', $(this).scrollTop() > $('.menu-upper').height());
+    $('.container').toggleClass('content-sticky', $(this).scrollTop() > $('.menu-upper').height());
+});
